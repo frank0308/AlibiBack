ea94a55c3249994ac0a9644498b1f37536728a5b
Aus <abc2614856@gmail.com>
2020-10-26 21:23:31 +0800

    加入專案檔案。

---
+++ AlibiScript/Interface/IRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AlibiScript.Interface
+{
+    interface IRepository<TEntity>
+    {
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <param name="entity">實體</param>
+        void Create(TEntity entity); //void可改TKey
+
+
+        /// <summary>
+        /// 取得全部
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        /// <summary>
+        /// 特定條件取得取得
+        /// </summary>
+        /// <param name="expression">查詢條件</param>
+        /// <returns></returns>
+        Task<TEntity> Get(Expression<Func<TEntity, bool>> expression);
+
+        /// <summary>
+        /// 刪除
+        /// </summary>
+        void Delete(TEntity entity);
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="entity">實體</param>
+        void Update(TEntity entity);
+    }
+}
---
+++ AlibiScript/Repository/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using AlibiScript.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AlibiScript.Repository
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly object DBcontext;
+        protected object Context { get { return DBcontext; } }
+        public GenericRepository(object DB) {
+            DBcontext = DB;
+        }
+
+        public void Create(TEntity entity)
+        {
+
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> Get(Expression<Func<TEntity, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
